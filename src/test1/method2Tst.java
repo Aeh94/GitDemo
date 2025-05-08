cc1b2d4b44ea13ae0f87b58bedb4383b516cfaeb
anas <elhadrami.anas@gmail.com>
2025-05-08 20:41:44 +0200

    new methods Achitect guy

--- src/test1/method2Tst.java
+++ src/test1/method2Tst.java
@@ -8,4 +8,16 @@ public class method2Tst {
 		System.out.println("ana lfeeeen");
 		System.out.println("ana lghzaal");
 	}
+	
+	public void nupty() {
+		
+		System.out.println("appium here");
+		System.out.println("anry");
+	}
+	
+	public void naughty() {
+		
+		System.out.println("selenium");
+		System.out.println("sylabus");
+	}
 }
